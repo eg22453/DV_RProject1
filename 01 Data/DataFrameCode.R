992b86d05f04cf9f5d4f4ec9d05a681d107f0cb3
bdehoyos <bruno@wireless-10-145-103-163.public.utexas.edu>
2015-02-04 15:41:59 -0600

    Added more .r files, should be near done

--- 01 Data/DataFrameCode.R
+++ 01 Data/DataFrameCode.R
@@ -1,20 +1,4 @@
-library("ggplot2", lib.loc="/Library/Frameworks/R.framework/Versions/3.1/Resources/library")
-library("grid", lib.loc="/Library/Frameworks/R.framework/Versions/3.1/Resources/library")
-library("plyr", lib.loc="/Library/Frameworks/R.framework/Versions/3.1/Resources/library")
-library("reshape2", lib.loc="/Library/Frameworks/R.framework/Versions/3.1/Resources/library")
-library("RCurl", lib.loc="/Library/Frameworks/R.framework/Versions/3.1/Resources/library")
-library("gplots", lib.loc="/Library/Frameworks/R.framework/Versions/3.1/Resources/library")
-
-
 df_c <- data.frame(eval(parse(text=substring(getURL(URLencode('http://129.152.144.84:5001/rest/native/?query="select * from customers"'), httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521/PDB1.usuniversi01134.oraclecloud.internal', USER='DV_ORDERS', PASS='orcl', MODE='native_mode', MODEL='model', returnFor = 'R', returnDimensions = 'False'), verbose = TRUE), 1, 2^31-1))))
 df_i <- data.frame(eval(parse(text=substring(getURL(URLencode('http://129.152.144.84:5001/rest/native/?query="select * from items"'), httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521/PDB1.usuniversi01134.oraclecloud.internal', USER='DV_ORDERS', PASS='orcl', MODE='native_mode', MODEL='model', returnFor = 'R', returnDimensions = 'False'), verbose = TRUE), 1, 2^31-1))))
 df_o <- data.frame(eval(parse(text=substring(getURL(URLencode('http://129.152.144.84:5001/rest/native/?query="select * from orders"'), httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521/PDB1.usuniversi01134.oraclecloud.internal', USER='DV_ORDERS', PASS='orcl', MODE='native_mode', MODEL='model', returnFor = 'R', returnDimensions = 'False'), verbose = TRUE), 1, 2^31-1))))
 df_od <- data.frame(eval(parse(text=substring(getURL(URLencode('http://129.152.144.84:5001/rest/native/?query="select * from order_details"'), httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521/PDB1.usuniversi01134.oraclecloud.internal', USER='DV_ORDERS', PASS='orcl', MODE='native_mode', MODEL='model', returnFor = 'R', returnDimensions = 'False'), verbose = TRUE), 1, 2^31-1))))
-
-df_c
-
-df_i
-
-df_o
-
-df_od
---
+++ 01 Data/DataFrameShow.R
@@ -0,0 +1,7 @@
+head(df_c)
+
+head(df_i)
+
+head(df_o)
+
+head(df_od)
